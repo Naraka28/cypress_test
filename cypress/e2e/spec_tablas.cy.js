5d15ca1e67cfafc2ccc5e372f839125649752d80
Gael Borchardt <gaelborchardt@gmail.com>
2024-11-06 14:51:13 -0700

    actu

--- cypress/e2e/spec_tablas.cy.js
+++ cypress/e2e/spec_tablas.cy.js
@@ -16,32 +16,48 @@ const pages = [
 
 pages.forEach(({ name, url }) => {
     it(`should navigate to ${name}, check tables`, () => {
-        cy.get(`.no-underline.text-black[href=${url}]`).click()
+        cy.get(`.no-underline[href=${url}]`).click()
        // cy.url().should('endWith', url);
        
-       cy.get('.MuiTable-root.css-1ks3uzw-MuiTable-root').should('exist')
+       cy.get('.MuiTable-root').should('exist')
     
     });
 });
 
 pages.forEach(({ name, url }) => {
     it(`should navigate to ${name}, check edit button`, () => {
-        cy.get(`.no-underline.text-black[href=${url}]`).click()
+        cy.get(`.no-underline[href=${url}]`).click()
        // cy.url().should('endWith', url);
-       cy.get('.MuiTable-root.css-1ks3uzw-MuiTable-root').eq(1).find('.btnUpdate').click(); // Selecciona el botón "EDIT" en la primera fila
+       cy.get('.MuiTable-root').eq(1).find('.btnUpdate').click(); // Selecciona el botón "EDIT" en la primera fila
        cy.get('.bg-blue-500').click() //guardar sin cambiar nada
     });
 });
 
 pages.forEach(({ name, url }) => {
     it(`should navigate to ${name}, check delete button`, () => {
-        cy.get(`.no-underline.text-black[href=${url}]`).click()
+        cy.get(`.no-underline[href=${url}]`).click()
        // cy.url().should('endWith', url);
-       cy.get('.MuiTable-root.css-1ks3uzw-MuiTable-root').eq(1).find('.btnDelete').click(); // Selecciona el botón "EDIT" en la primera fila
+       cy.get('.MuiTable-root').eq(1).find('.btnDelete').click(); // Selecciona el botón "EDIT" en la primera fila
      
        cy.get('.text-red-500').click() //cancelar
     });
 });
+
+pages.forEach(({ name, url }) => {
+        if(name === 'Products'){
+            it(`should navigate to ${name}, check edit button`, () => {
+                cy.get(`.no-underline[href=${url}]`).click()
+               // cy.url().should('endWith', url);
+               cy.get('.MuiTable-root').eq(1).find('.btnUpdate').click(); // Selecciona el botón "EDIT" en la primera fila
+               cy.get('#cantidad').type('10')
+               cy.get('.bg-blue-500').click() //guardar sin cambiar nada
+            });
+        }
+    });
+
+
+
+
 
 
 
